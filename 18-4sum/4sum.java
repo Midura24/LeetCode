3dbf8979ac6402b3ab62fb87aec53c35bc2171e9
Midura24 <midura2411@gmail.com>
2025-05-31 20:22:54 +0530

    Time: 16 ms (81.57%) | Memory: 43.9 MB (63.49%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,9 +1,8 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-
-        List<List<Integer>> lst=new ArrayList<>();
         int n=nums.length;
         Arrays.sort(nums);
+        List<List<Integer>> lst = new ArrayList<>();
         for(int i=0;i<n;i++)
         {
             if(i>0 && nums[i]==nums[i-1])
@@ -11,8 +10,8 @@ class Solution {
                 continue;
             }
             for(int j=i+1;j<n;j++)
-            {    
-                if(j>i+1 && nums[j]==nums[j-1])
+            {
+                 if(j>i+1 && nums[j]==nums[j-1])
                 {
                     continue;
                 }
@@ -21,17 +20,18 @@ class Solution {
 
                 while(k<l)
                 {
-                    long sum=+nums[i];
+                    long sum=nums[i];
                     sum+=nums[j];
                     sum+=nums[k];
                     sum+=nums[l];
-                    if(sum<target)
+                    if(sum==target)
                     {
-                        k++;
-                    }
-                     else if(sum==target)
-                    {
-                        List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
+
                         lst.add(temp);
                         k++;
                         l--;
@@ -45,8 +45,11 @@ class Solution {
                             l--;
                         }
                     }
-                    else
+                    if(sum<target)
                     {
+                        k++;
+                    }
+                    else if(sum>target){
                         l--;
                     }
                 }
