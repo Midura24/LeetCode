55429981fec0656f0eef4c00a934972063c93c92
Midura24 <midura2411@gmail.com>
2025-07-10 22:07:41 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (25.05%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        double ans = 1.0;
+        long num = n; // Use long to handle edge case like Integer.MIN_VALUE
+
+        if (num < 0) {
+            x = 1 / x;
+            num = -num;
+        }
+
+        while (num > 0) {
+            if (num % 2 == 1) {
+                ans *= x;
+            }
+            x *= x;
+            num /= 2;
+        }
+
+        return ans;
     }
 }
